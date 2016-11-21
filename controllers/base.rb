038615401e6c16e31bb152fdf94b24ddb082884a
Sami Laaroussi Tribek <sami@slt.fr>
2016-11-22 01:00:11 +0800

    init and base for controller

---
+++ controllers/base.rb
@@ -0,0 +1,15 @@
+class DevRankAPP < Sinatra::Base
+  extend Econfig::Shortcut
+
+  configure do
+    Econfig.env = settings.environment.to_s
+    Econfig.root = File.expand_path('..', settings.root)
+  end
+
+  set :views, File.expand_path('../../views', __FILE__)
+  set :public_dir, File.expand_path('../../public', __FILE__)
+
+  after do
+    content_type 'text/html'
+  end
+end
