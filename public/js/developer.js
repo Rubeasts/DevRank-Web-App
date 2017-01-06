f4f1ff589d1e4cdc160bc42e7a073e645ba275c7
Sami Laaroussi Tribek <sami@slt.fr>
2017-01-06 23:25:35 +0800

    static data from slim

--- public/js/developer.js
+++ public/js/developer.js
@@ -1,16 +1,40 @@
-var buyers = document.getElementById('myChart').getContext('2d');
+function resize(){
+    $("#canvas").outerHeight($(window).height()-$("#canvas").offset().top- Math.abs($("#canvas").outerHeight(true) - $("#canvas").outerHeight()));
+  }
+  $(document).ready(function(){
+    resize();
+    $(window).on("resize", function(){
+        resize();
+    });
+  });
 
-var buyerData = {
-	labels : ["January","February","March","April","May","June"],
-	datasets : [
-		{
-			fillColor : "rgba(172,194,132,0.4)",
-			strokeColor : "#ACC26D",
-			pointColor : "#fff",
-			pointStrokeColor : "#9DB86D",
-			data : [203,156,99,251,305,247]
-		}
-	]
-}
+var chartData = {
+    labels: ["Flog", "Flai", "Rubocop"],
+    datasets: [
+        {
+            fillColor: "#79D1CF",
+            strokeColor: "#79D1CF",
+            data: [flog, flai, rubo]
+        }
+    ]
+};
+var ctx = document.getElementById("myChart").getContext("2d");
+var myBar = new Chart(ctx).Bar(chartData, {
+    showTooltips: false,
+    onAnimationComplete: function () {
+
+        var ctx = this.chart.ctx;
+        ctx.font = this.scale.font;
+        ctx.fillStyle = this.scale.textColor
+        ctx.textAlign = "center";
+        ctx.textBaseline = "bottom";
+
+        this.datasets.forEach(function (dataset) {
+            dataset.bars.forEach(function (bar) {
+                ctx.fillText(bar.value, bar.x, bar.y - 5);
+            });
+        })
+    }
+});
 
 new Chart(buyers).Line(buyerData);
