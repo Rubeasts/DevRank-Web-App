dd56f4641c3745f232bead51c1fbb507395cd1ca
Sami Laaroussi Tribek <sami@slt.fr>
2017-01-09 05:56:23 +0800

    load participation for all users

--- public/js/developer.js
+++ public/js/developer.js
@@ -3,17 +3,19 @@
       labels.push(i);
   }
 
-  var part_all = [];
+  function loadParticipation(a) {
 
-  var buyerData = {
-  	labels : labels,
-  	datasets : [
-  		{
-  			fillColor : "rgba(172,194,132,0.4)",
-  			strokeColor : "#ACC26D",
-  			pointColor : "#fff",
-  			pointStrokeColor : "#9DB86D",
-  			data : part_all
-  		}
-  	]
+    var buyerData = {
+    	labels : labels,
+    	datasets : [
+    		{
+    			fillColor : "rgba(172,194,132,0.4)",
+    			strokeColor : "#ACC26D",
+    			pointColor : "#fff",
+    			pointStrokeColor : "#9DB86D",
+    			data : a
+    		}
+    	]
+    }
+    return buyerData;
   }
