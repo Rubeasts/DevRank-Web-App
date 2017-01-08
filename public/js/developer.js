9650e657ace20eca0aa2ac47a25a9c435d6f45a7
Sami Laaroussi Tribek <sami@slt.fr>
2017-01-08 22:37:49 +0800

    index property for stat participation

--- public/js/developer.js
+++ public/js/developer.js
@@ -8,33 +8,17 @@ function resize(){
     });
   });
 
-var chartData = {
-    labels: ["Flog", "Flai", "Rubocop"],
-    datasets: [
-        {
-            fillColor: "#79D1CF",
-            strokeColor: "#79D1CF",
-            data: [flog, flai, rubo]
-        }
-    ]
-};
-var ctx = document.getElementById("myChart").getContext("2d");
-var myBar = new Chart(ctx).Bar(chartData, {
-    showTooltips: false,
-    onAnimationComplete: function () {
 
-        var ctx = this.chart.ctx;
-        ctx.font = this.scale.font;
-        ctx.fillStyle = this.scale.textColor
-        ctx.textAlign = "center";
-        ctx.textBaseline = "bottom";
 
-        this.datasets.forEach(function (dataset) {
-            dataset.bars.forEach(function (bar) {
-                ctx.fillText(bar.value, bar.x, bar.y - 5);
-            });
-        })
-    }
-});
-
-new Chart(buyers).Line(buyerData);
+  var buyerData = {
+  	labels : ["January","February","March","April","May","June"],
+  	datasets : [
+  		{
+  			fillColor : "rgba(172,194,132,0.4)",
+  			strokeColor : "#ACC26D",
+  			pointColor : "#fff",
+  			pointStrokeColor : "#9DB86D",
+  			data : [203,156,99,251,305,247]
+  		}
+  	]
+  }
